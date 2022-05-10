44add45b8648e2d01c966442435da0df1178fc58
NunaHajima <nikenwidyaa1210@gmail.com>
2022-05-11 01:20:00 +0700

    revisi

--- Object_Orient1-3/Inheritance/src/main/java/com/mycompany/inheritance/Inheritance.java
+++ Object_Orient1-3/Inheritance/src/main/java/com/mycompany/inheritance/Inheritance.java
@@ -12,6 +12,14 @@ class Merchandise extends KPOPFan {
   public void harga2(){
       System.out.println(100000);
   }
+  private final String JenisMerchandise2 = ("Merchandise 2(Baju) : "); 
+  public void harga3(){
+      System.out.println(1000000);
+  }
+  private final String JenisMerchandise3 = ("Merchandise 3(Note Book) : "); 
+  public void harga4(){
+      System.out.println(200000);
+  }
   public static void main(String[] args) {
     Merchandise Merc = new Merchandise();
     System.out.println(Merc.barang + " ");
@@ -19,5 +27,11 @@ class Merchandise extends KPOPFan {
     System.out.println(" ");
     System.out.println(Merc.JenisMerchandise + " ");
     Merc.harga2();
+    System.out.println(" ");
+    System.out.println(Merc.JenisMerchandise2 + " ");
+    Merc.harga3();
+    System.out.println(" ");
+    System.out.println(Merc.JenisMerchandise3 + " ");
+    Merc.harga4();
   }
 }
